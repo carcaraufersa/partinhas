c2347f2997fad76f1331bded885cc0a45236c079
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2026-05-28 14:47:23 -0300

    feat: Desgin fnializado. Compontenes criados

--- unidosPorPatinhas/src/components/InputText/index.tsx
+++ unidosPorPatinhas/src/components/InputText/index.tsx
@@ -4,7 +4,7 @@ import { styles } from "./styles";
 export function InputText({ ...rest }: TextInputProps) {
     return <TextInput
         style={styles.container}
-        placeholderTextColor={"#202020"}
+        placeholderTextColor={"#555555"}
         { ...rest }
     />
 }
